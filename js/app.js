a7046878f3b07c1b0cf6284db2770ac8de91091c
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-01 15:56:15 -0500

    attempt functionality for clicks and array iteration

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const winningCombos = [ 
+const winningCombos = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -9,10 +9,10 @@ const winningCombos = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
-]
+];
 
 /*---------------------------- Variables (state) ----------------------------*/
-let tttBoard, turn, winner
+let tttBoard, turn, winner;
 
 // let sq0, sq1, sq2, sq3, sq4, sq5, sq6, sq7, sq8
 // sq0 = document.getElementById("sq0").value
@@ -25,67 +25,69 @@ let tttBoard, turn, winner
 // sq7 = document.getElementById("sq7").value
 // sq8 = document.getElementById("sq8").value
 
-
 /*------------------------ Cached Element References ------------------------*/
-const squareEls = document.querySelectorAll('div')
-const resetBtn = document.getElementById('reset-btn')
+const squareEls = document.querySelectorAll(".cell");
+// const resetBtn = document.getElementById('reset-btn')
 
-let messageEl = document.getElementById('message');
+let messageEl = document.getElementById("message");
 
 /*----------------------------- Event Listeners -----------------------------*/
 // get click from boxes
-squareEls.addEventListener('click', function(evt) {
+document.querySelector("section.board").addEventListener("click", handleClick);
 
-})
-
-// })
 // get click from start/reset
-resetBtn.addEventListener('click', function(event){
-  //
-})
-
+// resetBtn.addEventListener('click', function(event){
+//   //
+// })
 
 /*-------------------------------- Functions --------------------------------*/
 
-
 function init() {
-  tttBoard = [null, null, null, null, null, null, null, null, null]
-  turn = 1
-  render()
-  winner = null
-  console.log('Game loaded')
-  // console.log(squareEls)
-  messageEl.textContent = `Player ${turn === 1 ? 1 : 2}, your move`
+  tttBoard = [null, null, null, null, null, null, null, null, null];
+  turn = 1;
+  render();
+  console.log("Game loaded");
+  messageEl.textContent = `Player ${turn === 1 ? 1 : 2}, your move`;
 }
 
 function handleClick(evt) {
-  turn *= -1
-  render()
-  console.log(turn)
-  console.log('click')
+  // tttBoard.addEventListener('click', handleClick)
+  if (tttBoard[idx] !== null) {
+    return;
+  }
+  turn *= -1;
+  render();
+  console.log(turn);
+  console.log("click");
 }
 
 function render() {
-  if (winner === 'T') {
-    messageEl.textContent = "It's a tie! Play again?"
+  let sqIndex;
+  tttBoard.forEach(function (element, idx) {
+    if (turn === 1) {
+      // code to access the element at the matching index in the squareEls array and set it's textContent to 'X'
+    } else {
+      // code to access the element at the matching index in the squareEls array and set it's textContent to 'O'
+    }
+  });
+  if (winner === "T") {
+    messageEl.textContent = "It's a tie! Play again?";
   } else if (winner === 1) {
-    messageEl.textContent = 'Congratulations! Player 1 has won!'
+    messageEl.textContent = "Congratulations! Player 1 has won!";
   } else if (winner === -1) {
-    messageEl.textContent = 'Congratulations! Player 2 has won!'
+    messageEl.textContent = "Congratulations! Player 2 has won!";
   }
-  if ((winner === 1) || (winner === -1) || (winner === 'T')){
-    resetBtn.removeAttribute('hidden')
+  if (winner === 1 || winner === -1 || winner === "T") {
+    resetBtn.removeAttribute("hidden");
   }
-  console.log(winner)
-  console.log(turn)
-
 }
 
-function getWinner(){
+// function reset() {
 
-}
-function reset() {
+// }
 
-}
+init();
 
-init()
+console.log(winner);
+console.log(turn);
+console.log(squareEls);
