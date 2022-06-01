eb43caa724e4c62ebea762565926d58fd75c17c9
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-01 12:50:04 -0500

    fix render message

--- js/app.js
+++ js/app.js
@@ -2,59 +2,64 @@
 const players = {
 
 }
-const squareEls = []
-
-const messageEl = document.getElementById('message')
+const winningCombos = []
 
 /*---------------------------- Variables (state) ----------------------------*/
-let board, turn, winner
+let tttBoard, turn, winner
 
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.getElementById('sq0')
+const resetBtn = document.getElementById('reset-btn')
 
-
+let messageEl = document.getElementById('message')
 
 /*----------------------------- Event Listeners -----------------------------*/
 // get click from boxes
-squareContainer.addEventListener('click', handleClick)
+squareEls.addEventListener('click', handleClick)
 // get click from start/reset
-.reset-btn.addEventListener('click', handleClick)
+resetBtn.addEventListener('click', handleClick)
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
 function init() {
-  board = [null, null, null, null, null, null, null, null, null]
+  tttBoard = [null, null, null, null, null, null, null, null, null]
   turn = 1
   render()
   winner = null
+  console.log('Game loaded')
 }
 function handleClick() {
   //check is square has already been clicked
   //if so, nothing
   //if no, change to corresponding player signifier
   turn *= -1
-  render()
+  // render()
 }
 
 function render() {
-  board.forEach(element => {
+  tttBoard.forEach(element => {
     
   });
   if (turn = 1) {
-    messageEl = 'Player 1, your move!'
+    messageEl.textContent = 'Player 1, your move!'
   } else if (turn = -1) {
-    messageEl = 'Player 2, your move!'
+    messageEl.textContent = 'Player 2, your move!'
   }
   if (winner === 'T') {
-    messageEl = "It's a tie! Play again?"
+    messageEl.textContent = "It's a tie! Play again?"
   } else if (winner = 1){
-    messageEl = 'Congratulations! Player 1 has won!'
+    messageEl.textContent = 'Congratulations! Player 1 has won!'
   } else if (winner = -1) {
-    messageEl = 'Congratulations! Player 2 has won!'
+    messageEl.textContent = 'Congratulations! Player 2 has won!'
   }
+  console.log(winner)
+  console.log(turn)
+
 }
 
 function reset() {
-
+  
 }
