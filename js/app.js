e023f11ac6d506c91ef94f4a3a6a99e0df8cb323
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-01 21:44:04 -0500

    make reset functional and clean up a bit

--- js/app.js
+++ js/app.js
@@ -27,8 +27,8 @@ let tttBoard, turn, winner;
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll(".cell");
-const resetBtn = document.getElementById('reset-btn')
-const board = document.querySelector('div.cell')
+const resetBtn = document.getElementById("reset-btn");
+const board = document.querySelector("div.cell");
 let messageEl = document.getElementById("message");
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -36,7 +36,7 @@ let messageEl = document.getElementById("message");
 document.querySelector("section.board").addEventListener("click", handleClick);
 
 // get click from start/reset
-resetBtn.addEventListener('click', reset)
+resetBtn.addEventListener("click", reset);
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -45,29 +45,24 @@ function init() {
   turn = 1;
   render();
   console.log("Game loaded");
-  messageEl.textContent = 'Player 1, start the game!'
+  messageEl.textContent = "Player 1, start the game!";
 }
 
 function handleClick(evt) {
-  
-  turn *= -1
+  turn *= -1;
   messageEl.textContent = `Player ${turn === 1 ? 1 : 2}, your move`;
-  render()
-  console.log(turn)
-  console.log("click")
+  render();
+  console.log(turn);
+  console.log("click");
 }
 
 function render() {
   tttBoard.forEach(function (element, idx) {
     if (turn === 1) {
-      // code to access the element at the matching index in the squareEls array and set it's textContent to 'X'
-      squareEls[idx].textContent = 'X'
+      squareEls[idx].textContent = "X";
     } else {
-      // code to access the element at the matching index in the squareEls array and set it's textContent to 'O'
-      squareEls[idx].textContent= 'O'
+      squareEls[idx].textContent = "O";
     }
-
-
   });
   if (winner === "T") {
     messageEl.textContent = "It's a tie! Play again?";
@@ -76,13 +71,22 @@ function render() {
   } else if (winner === -1) {
     messageEl.textContent = "Congratulations! Player 2 has won!";
   }
-  if (winner!= null) {
+  if (winner != null) {
     resetBtn.removeAttribute("hidden");
   }
 }
 
-function reset() {
+function getWinner() {
+  winningCombos.forEach((combo) => {
+    // find the sum of the values within the board array at indices 0, 1, and 2 of the combo
+    // if the absolute value of the sum is 3, there's a winner
+    // because you're tracking the turn, you can determine the winner using that variable, which is all you need to return
+  });
+}
 
+function reset() {
+  init();
+  resetBtn.setAttribute('hidden', true)
 }
 
 init();
