fb2615b9e7abb9f92759552b105bdb27f6d9fd3c
Nicholas Bailey <nickcyg1992@gmail.com>
2022-06-02 16:30:39 -0500

    fix win function, fix render, fix reset

--- js/app.js
+++ js/app.js
@@ -12,61 +12,53 @@ const winningCombos = [
 ];
 
 /*---------------------------- Variables (state) ----------------------------*/
-let tttBoard, turn, winner;
+let board, turn, winner;
 
 /*------------------------ Cached Element References ------------------------*/
 const squareEls = document.querySelectorAll(".cell");
 const resetBtn = document.getElementById("reset-btn");
-const board = document.querySelector("div.cell");
 let messageEl = document.getElementById("message");
 
 /*----------------------------- Event Listeners -----------------------------*/
 // get click from boxes
-document.querySelector("section.board").addEventListener("click", handleClick);
+// document.querySelector("section.board").addEventListener("click", handleClick);
 
+squareEls.forEach((square) => {
+  square.addEventListener("click", handleClick);
+});
 // get click from start/reset
 resetBtn.addEventListener("click", reset);
 
 /*-------------------------------- Functions --------------------------------*/
 
 function init() {
-  tttBoard = [null, null, null, null, null, null, null, null, null];
+  board = [null, null, null, null, null, null, null, null, null];
   turn = 1;
-  render();
+  winner = null;
   console.log("Game loaded");
   // messageEl.textContent = "Player 1, start the game!";
-  messageEl.textContent = `Player ${turn === 1 ? '1' : '2'}, your move`;
+  messageEl.textContent = `Player 1, your move`;
+  render();
 }
 
 function handleClick(evt) {
-  let sqIdx = parseInt(evt.target.id.substring(2))
-  tttBoard[sqIdx] = turn;
-  // if (evt[sqIdx] !== null || winner !== null) {
-  //     return null
-  // }
-  render();
-  console.log(tttBoard[sqIdx]);
-  console.log(tttBoard);
+  let sqIdx = parseInt(evt.target.id.replace("sq", ""));
+  if (board[sqIdx] !== null) return;
+  board[sqIdx] = turn;
+  winner = getWinner();
   turn *= -1;
-  console.log(evt.target.id.substring(2))
+  render();
 }
-
-// function handleClick(evt) {
-//   let sqIdx = evt.target.id.substring
-//   turn *= -1
-//   messageEl.textContent = `Player ${turn === 1 ? 1 : 2}, your move`;
-//   winner = getWinner()
-//   render()
-// }
 
 function render() {
   squareEls.forEach(function (num, idx) {
-    if (tttBoard[idx] === 1) {
-      num.textContent = "X";
-    } else if (tttBoard[idx] === -1) {
-      num.textContent = "O";
+    if (board[idx] === 1) {
+      num.textContent = "x";
+    } else if (board[idx] === -1) {
+      num.textContent = "o";
+    } else if (board[idx] === null) {
+      num.textContent = ''
     }
-    // console.log(tttBoard[num].textContent)
   });
   if (winner === "T") {
     messageEl.textContent = "It's a tie! Play again?";
@@ -75,38 +67,37 @@ function render() {
   } else if (winner === -1) {
     messageEl.textContent = "Congratulations! Player 2 has won!";
   }
-  if (winner != null) {
+  if (winner !== null) {
     resetBtn.removeAttribute("hidden");
   }
-  messageEl.textContent = `Player ${turn === -1 ? '1' : '2'}, your move`;
+  messageEl.textContent = `Player ${turn === 1 ? "1" : "2"}, your move`;
 }
 
 function getWinner() {
-  winningCombos.forEach((combo) => {
-    // find the sum of the values within the board array at indices 0, 1, and 2 of the combo
-    // if the absolute value of the sum is 3, there's a winner
-    // because you're tracking the turn, you can determine the winner using that variable, which is all you need to return
-  });
+  for (let i = 0; i < winningCombos.length; i++) {
+    if (
+      board[winningCombos[i][0]] +
+        board[winningCombos[i][1]] +
+        board[winningCombos[i][2]] ===
+      3
+    ) {
+      return 1;
+    } else if (
+      board[winningCombos[i][0]] +
+        board[winningCombos[i][1]] +
+        board[winningCombos[i][2]] ===
+      -3
+    ) {
+      return -1;
+    }
+  }
+  return null;
 }
 
-// function getWinner() {
-//   for (let i=0; i < winningCombos.length; i++) {
-//       if (board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]] === 3) {
-//           return 1
-//       } else  if (board[winningCombos[i][0]] + board[winningCombos[i][1]] + board[winningCombos[i][2]] === -3) {
-//           return -1
-//       }
-//   }
-//   return null
-// }
-
 function reset() {
+  // board = [null, null, null, null, null, null, null, null, null];
   init();
-  tttBoard = [null, null, null, null, null, null, null, null, null]
   resetBtn.setAttribute("hidden", true);
 }
 
 init();
-
-console.log(winner);
-console.log(turn);
